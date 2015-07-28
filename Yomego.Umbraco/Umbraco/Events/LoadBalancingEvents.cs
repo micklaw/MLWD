6d91f629193e19590d726ce9844f412554df44bf
Michael Law <michael@michaellaw.me>
2015-07-28 22:55:19 +0100

    updated to umbraco 7.3.0-beta2

---
+++ Yomego.Umbraco/Umbraco/Events/LoadBalancingEvents.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core;
+using Umbraco.Core.Services;
+using Umbraco.Core.Sync;
+using Umbraco.Web;
+
+namespace Yomego.Umbraco.Umbraco.Events
+{
+    public class LoadBalancingEvents : ApplicationEventHandler
+    {
+        protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            ServerMessengerResolver.Current.SetServerMessenger(new BatchedDatabaseServerMessenger(
+                applicationContext,
+                true,
+                new DatabaseServerMessengerOptions
+                {
+                    InitializingCallbacks = new Action[]
+				{
+					() => global::umbraco.content.Instance.RefreshContentFromDatabase(),
+					() => Examine.ExamineManager.Instance.RebuildIndex()
+				}
+                }));
+
+            ServerRegistrarResolver.Current.SetServerRegistrar(new DatabaseServerRegistrar(new Lazy<IServerRegistrationService>(() => applicationContext.Services.ServerRegistrationService), new DatabaseServerRegistrarOptions()));
+        }
+    }
+}
