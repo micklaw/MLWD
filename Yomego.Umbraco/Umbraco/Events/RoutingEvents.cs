4d21cc5637e72609008396c4cbb96165d77d335b
Michael Law <michael@michaellaw.me>
2015-10-01 23:03:39 +0100

    updating to 7.3.0 and new Yomgego framework

---
+++ Yomego.Umbraco/Umbraco/Events/RoutingEvents.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core;
+using Umbraco.Web.Routing;
+using Yomego.Umbraco.Umbraco.Routing;
+
+namespace Yomego.Umbraco.Umbraco.Events
+{
+    public class RoutingEvents : ApplicationEventHandler
+    {
+        protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
+        {
+            ContentFinderResolver.Current.InsertTypeBefore<ContentFinderByNiceUrl, CustomContentFinder>();
+            ContentFinderResolver.Current.RemoveType<ContentFinderByNiceUrl>();
+
+            base.ApplicationStarting(umbracoApplication, applicationContext);
+        }
+    }
+}
